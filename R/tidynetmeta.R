f525cb654735b88161eb8f72c656045f3a8b014f
QuietResearcher <96798595+QuietResearcher@users.noreply.github.com>
2021-12-28 17:49:06 -0500

    Basic Functions Implemented

--- R/tidynetmeta.R
+++ R/tidynetmeta.R
@@ -1,3 +1,5 @@
+library (netmeta)
+
 # Hello, world!
 #
 # This is an example function named 'hello'
@@ -13,6 +15,32 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-runmeta <- function() {
-  print("Hello, world!")
+simpleNMA <- function(data, n=NULL, r=NULL, measure = "OR", correction = FALSE, random = TRUE, ref = "Untreated", sm.val = "good") {
+
+  if (!is.null(n) && !is.null(r)) {
+    transformed_data <- pairwise(treat = treatment, event = data$r, studlab = study, n = data$n, data = data, sm = measure, allstudies = correction)
+  } else if(!is.null(n)) {
+    transformed_data <- pairwise(treat = treatment, event = responders, studlab = study, n = data$n, data = data, sm = measure, allstudies = correction)
+  } else if (!is.null(r)) {
+    transformed_data <- pairwise(treat = treatment, event = data$r, studlab = study, n = sampleSize, data = data, sm = measure, allstudies = correction)
+  } else {
+    transformed_data <- pairwise(treat = treatment, event = responders, studlab = study, n = sampleSize, data = data, sm = measure, allstudies = correction)
+  }
+
+
+  c.random <- random
+  c.fixed <- !random
+
+  NMAobject <- netmeta(TE, seTE, treat1, treat2, studlab, data = transformed_data, sm = measure, comb.fixed = c.fixed, comb.random = c.random,
+                       prediction = FALSE, reference.group = ref, small.values = sm.val)
+
+  return (NMAobject)
+
+}
+
+NMAsummary <- function(NMAobj) {
+
+  forest(result.bin, leftcols = c("studlab", "Pscore"),xlim=c(0.01,100), sortvar = -Pscore, plotwidth="10cm", layout = "JAMA")
+  print (ref = NMAobj$reference.group, NMAobj)
+
 }
